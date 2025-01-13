df49a7a4445887ac765fdc8cd72b48d3ef72515d
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2025-01-13 19:22:22 -0300

    Update package.json and package-lock.json to include Prisma dependency; modify middleware path matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: ["/admin/:path*", "/api/:path*", "/calculator"], 
+  matcher: ["/registration/:path*", "/api/:path*", "/calculator"], 
 }
