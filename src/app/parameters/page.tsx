051d08b5ab0604404c6141d9e3ffed5573619323
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2025-01-31 14:46:02 -0300

    Add localization support for ParametersPage; update titles and tab labels for internationalization

--- src/app/parameters/page.tsx
+++ src/app/parameters/page.tsx
@@ -2,16 +2,19 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { AllFilaments } from "../components/AllFilaments"
 import { AllPrinters } from "../components/AllPrinters"
 import { AllRegionsCost } from "../components/AllRegionsCost"
+import { useTranslations } from "next-intl"
 
 export default function ParametersPage() {
+  const t = useTranslations()
+
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Painel de Cadastros</h1>
+      <h1 className="text-2xl font-bold mb-4">{t('parameters.title')}</h1>
       <Tabs defaultValue="printers">
         <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="printers">Impressoras</TabsTrigger>
-          <TabsTrigger value="filaments">Filamentos</TabsTrigger>
-          <TabsTrigger value="regionCosts">Custos Regionais</TabsTrigger>
+          <TabsTrigger value="printers">{t('commom.printers')}</TabsTrigger>
+          <TabsTrigger value="filaments">{t('commom.filaments')}</TabsTrigger>
+          <TabsTrigger value="regionCosts">{t('commom.regions')}</TabsTrigger>
         </TabsList>
         <TabsContent value="printers">
           <AllPrinters />
@@ -26,4 +29,3 @@ export default function ParametersPage() {
     </div>
   )
 }
-
